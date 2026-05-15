98e1669a0b9e4404f55aa060f793a4166bbc0b58
IRockSolid <rocconota88@gmail.com>
2026-05-15 12:31:55 +0200

    fix(): Minor bug mix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import './app.css'
 import App from './App.svelte'
 
 const app = mount(App, {
-  target: document.getElementById('app'),
+  target: document.getElementById('app') ?? document.body, 
+  // ?? document.body Risolve l'errore usando il body come fallback se #app è null - anche se ora esiste il div
 })
 
 export default app
