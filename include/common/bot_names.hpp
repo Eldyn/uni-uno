dcf557cbaaea1a20e0bcc7461d4415c2b0e31764
Eldyn <oliva.alessandro.b@gmail.com>
2026-06-11 12:30:53 +0200

    feat: bot names list

---
+++ include/common/bot_names.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace game {
+
+inline const std::vector<std::string> kReservedBotNames = {
+    "GLaDOS", "HAL9000", "Bender", "Wall-E", "R2-D2", "C-3PO",
+    "Data", "Lore", "T-800", "Johnny5", "Marvin", "BB-8", "Ultron",
+    "Megatron", "Optimus", "Cortana", "Siri", "Alexa", "ChatGPT"
+};
+
+}
